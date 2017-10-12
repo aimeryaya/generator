00c9ed3cb56c2568bf1346eb555defd407d77f1a
aimeryaya <117468082@qq.com>
2017-10-12 20:17:37 +0800

    test desplay

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/plugins/CachePlugin.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/plugins/CachePlugin.java
@@ -47,6 +47,7 @@ import org.mybatis.generator.internal.util.StringUtility;
  * @author Jeff Butler
  */
 public class CachePlugin extends PluginAdapter {
+	//testdeploy......................
     public enum CacheProperty {
         EVICTION("cache_eviction", "eviction"), //$NON-NLS-1$ //$NON-NLS-2$
         FLUSH_INTERVAL("cache_flushInterval", "flushInterval"), //$NON-NLS-1$ //$NON-NLS-2$
